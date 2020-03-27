38d6dd4fe7bdec22c9119180f18fed5c84b94b02
Kartik Jain <Kartikjain-sudo>
2020-03-28 01:39:51 +0530

    Added README.md. It is an instabot which will automatically login to your account once you provided your username and password. I worked on selenium library along with chrome webdriver.

---
+++ README.md
@@ -0,0 +1,6 @@
+### My First Repository :)
+
+I'm Kartik Jain and here it's my first Github repository.
+If you see this file on my project, it's because I'm learning Git.
+
+#Happy_coding.... ;)
---
+++ instabot.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from time import sleep
+
+class InstaBot:
+    def __init__(self, username, pwd):
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://www.instagram.com/")
+        sleep(2)
+        self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input').send_keys(username)
+        self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input')send_keys(pwd)
+        self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]/button/div').click()
+        sleep(4)
+        self.driver.find_element_by_xpath('/html/body/div[4]/div/div/div[3]/button[2]').click()
+        
+InstaBot('username', 'password')
+
+
+
