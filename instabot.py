3992dcc93517d8dacd3e07c4ce4bf9ad7d4d8c0e
Kartik Jain <Kartikjain-sudo>
2020-03-28 11:39:23 +0530

    Second Commit

--- instabot.py
+++ instabot.py
@@ -7,7 +7,7 @@ class InstaBot:
         self.driver.get("https://www.instagram.com/")
         sleep(2)
         self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input').send_keys(username)
-        self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input')send_keys(pwd)
+        self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input').send_keys(pwd)
         self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]/button/div').click()
         sleep(4)
         self.driver.find_element_by_xpath('/html/body/div[4]/div/div/div[3]/button[2]').click()
